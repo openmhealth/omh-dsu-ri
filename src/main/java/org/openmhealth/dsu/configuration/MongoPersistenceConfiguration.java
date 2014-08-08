f56104bef367d55431546d78ce856fdd72fe7695
Emerson Farrugia <emerson.farrugia@gmail.com>
2014-08-08 17:35:49 +0200

    Add a Mongo persistence configuration.

---
+++ src/main/java/org/openmhealth/dsu/configuration/MongoPersistenceConfiguration.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2014 Open mHealth
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openmhealth.dsu.configuration;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+
+/**
+ * A configuration class that controls the Spring Data MongoDB repositories to scan for.
+ *
+ * @author Emerson Farrugia
+ */
+@Configuration
+@ConditionalOnExpression("${dataStore} == 'mongo'")
+@EnableMongoRepositories(basePackages = "org.openmhealth.dsu.repository")
+public class MongoPersistenceConfiguration {
+
+}
