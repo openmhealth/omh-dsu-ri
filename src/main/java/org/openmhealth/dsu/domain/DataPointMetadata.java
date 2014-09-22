09b75a651f5115747a536bfc2f565ea68f879a03
Emerson Farrugia <emerson.farrugia@gmail.com>
2014-09-22 12:38:35 +0200

    Update data point metadata to use schema IDs.

--- src/main/java/org/openmhealth/dsu/domain/DataPointMetadata.java
+++ src/main/java/org/openmhealth/dsu/domain/DataPointMetadata.java
@@ -16,7 +16,13 @@
 
 package org.openmhealth.dsu.domain;
 
+import org.openmhealth.schema.domain.SchemaId;
+
+import java.time.OffsetDateTime;
 import java.util.Optional;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 
 /**
@@ -27,32 +33,46 @@ import java.util.Optional;
 public class DataPointMetadata {
 
     private String id;
-    private String schemaName;
-    private String schemaVersion;
+    private SchemaId schemaId;
+    private OffsetDateTime creationTimestamp;
     private DataPointAcquisitionProvenance acquisitionProvenance;
 
     /**
-     * @param id the globally unique identifier of the data point
-     * @param schemaName the name of the schema the data point conforms to
-     * @param schemaVersion the version of the schema the data point conforms to
+     * @param id the identifier of the data point
+     * @param schemaId the identifier of the schema the data point conforms to
      */
-    public DataPointMetadata(String id, String schemaName, String schemaVersion) {
+    public DataPointMetadata(String id, SchemaId schemaId) {
+
+        this(id, schemaId, OffsetDateTime.now());
+    }
+
+    /**
+     * @param id the identifier of the data point
+     * @param schemaId the identifier of the schema the data point conforms to
+     * @param creationTimestamp the creation time of this data point
+     */
+    public DataPointMetadata(String id, SchemaId schemaId, OffsetDateTime creationTimestamp) {
+
+        checkNotNull(id);
+        checkArgument(!id.isEmpty());
+        checkNotNull(schemaId);
+        checkNotNull(creationTimestamp);
 
         this.id = id;
-        this.schemaName = schemaName;
-        this.schemaVersion = schemaVersion;
+        this.schemaId = schemaId;
+        this.creationTimestamp = creationTimestamp;
     }
 
     public String getId() {
         return id;
     }
 
-    public String getSchemaName() {
-        return schemaName;
+    public SchemaId getSchemaId() {
+        return schemaId;
     }
 
-    public String getSchemaVersion() {
-        return schemaVersion;
+    public OffsetDateTime getCreationTimestamp() {
+        return creationTimestamp;
     }
 
     /**
