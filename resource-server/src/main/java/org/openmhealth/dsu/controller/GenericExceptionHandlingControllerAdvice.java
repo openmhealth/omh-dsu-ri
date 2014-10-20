de7bc8bfe430b1fe86ceca90db114d3ccfa2712c
Emerson Farrugia <emerson.farrugia@gmail.com>
2014-10-20 10:36:15 +0200

    Add authentication-specific exception handling.

--- resource-server/src/main/java/org/openmhealth/dsu/controller/GenericExceptionHandlingControllerAdvice.java
+++ resource-server/src/main/java/org/openmhealth/dsu/controller/GenericExceptionHandlingControllerAdvice.java
@@ -19,9 +19,12 @@ package org.openmhealth.dsu.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 /**
@@ -34,11 +37,17 @@ public class GenericExceptionHandlingControllerAdvice {
 
     private static final Logger log = LoggerFactory.getLogger(GenericExceptionHandlingControllerAdvice.class);
 
+    @ExceptionHandler(AuthenticationCredentialsNotFoundException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public void handleAuthenticationCredentialsNotFoundException(Exception e, HttpServletRequest request) {
+
+        log.debug("A {} request for '{}' failed authentication.", request.getMethod(), request.getPathInfo(), e);
+    }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public void handleException(Exception e) {
+    public void handleException(Exception e, HttpServletRequest request) {
 
-        log.error("The following API exception is being swallowed to prevent it from being passed to the browser.", e);
+        log.debug("A {} request for '{}' failed.", request.getMethod(), request.getPathInfo(), e);
     }
 }
